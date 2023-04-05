5048bb164d4c8bfae265d643950eae295e74c9a3
gustavo <gustavo.isaac@gmail.com>
2023-04-05 15:31:53 +0100

    MnemosyneAlarmApplication

---
+++ app/src/main/java/com/artemissoftware/mnemosynealarm/MnemosyneAlarmApplication.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.mnemosynealarm
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MnemosyneAlarmApplication : Application()
